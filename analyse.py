5ad116af485007ff59fc334012ef44b36f4211e6
kashishh2 <kashishh2@gmail.com>
2018-05-26 01:21:18 +0530

    beta version

--- analyse.py
+++ analyse.py
@@ -5,6 +5,9 @@ from IPython.display import display
 import matplotlib.pyplot as plt
 import seaborn as sns
 from credential import *
+###########################################################
+from textblob import TextBlob
+import re
 #########################################%matplotlib inline
 
 def twitter_setup():
@@ -16,7 +19,22 @@ def twitter_setup():
     # Return API with authentication:
     api = tweepy.API(auth)
     return api
-############################################################
+####################################################################################################
+def clean_tweet(tweet):
+
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analize_sentiment(tweet):
+
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
+####################################################################################################
 extractor = twitter_setup()
 
 tweets = extractor.user_timeline(screen_name="realDonaldTrump", count=200)
@@ -27,4 +45,23 @@ print("5 recent tweets:\n")
 for tweet in tweets[:5]:
     print(tweet.text)
     print()
-##################################################################\
+##################################################################\################
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+###################################################################################
+data['len']  = np.array([len(tweet.text) for tweet in tweets])
+data['ID']   = np.array([tweet.id for tweet in tweets])
+data['Date'] = np.array([tweet.created_at for tweet in tweets])
+data['Source'] = np.array([tweet.source for tweet in tweets])
+data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
+data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
+##############################################################################################
+data['SA'] = np.array([ analize_sentiment(tweet) for tweet in data['Tweets'] ])
+##############################################################################################
+pos_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] > 0]
+neu_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == 0]
+neg_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] < 0]
+###############################################################################################
+print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data['Tweets'])))
+print("Percentage of neutral tweets: {}%".format(len(neu_tweets)*100/len(data['Tweets'])))
+print("Percentage de negative tweets: {}%".format(len(neg_tweets)*100/len(data['Tweets'])))
+###############################################################################################
