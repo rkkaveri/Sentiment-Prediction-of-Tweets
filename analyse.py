fa810f3ad0455ac9fa8d782f462e4635b23084ed
kashishh2 <kashishh2@gmail.com>
2018-05-26 00:53:15 +0530

    --

--- analyse.py
+++ analyse.py
@@ -4,4 +4,15 @@ import numpy as np
 from IPython.display import display
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
+from credential import *
+#########################################%matplotlib inline
+
+def twitter_setup():
+
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
